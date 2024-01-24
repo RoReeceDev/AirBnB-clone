520262aa10da8db7518144960edb1c0ffa2d22be
RoReeceDev <roreecedev@gmail.com>
2024-01-24 16:37:25 -0500

    passing object props into component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Star from '../../public/images/star.png'
 
 
-const Card = ({ coverImg, stats, price, description, title, location, openSpots, key }) => {
+const Card = (props) => {
+    const cardData = props.card
 
     let badgeText 
-    if(openSpots === 0){
+    if(cardData.openSpots === 0){
         badgeText = "Sold Out"
-    } else if(location === "Online"){
+    } else if(cardData.location === "Online"){
         badgeText="Online"
     }
 
@@ -16,16 +17,16 @@ const Card = ({ coverImg, stats, price, description, title, location, openSpots,
             {badgeText && <div className="card-badge">
                 {badgeText}
             </div>}
-            <img className="card-feature" src={`/images/${coverImg}`} alt="Main Card Image." />
+            <img className="card-feature" src={`/images/${cardData.coverImg}`} alt="Main Card Image." />
             <div className="card-rating">
                 <img className="card-star" src={Star} alt="Rating Star Icon." />
-                <span>{stats.rating}</span>
-                <span className="gray">({stats.reviewCount}) &bull;</span>
-                <span className="gray">{location}</span>
+                <span>{cardData.stats.rating}</span>
+                <span className="gray">({cardData.stats.reviewCount}) &bull;</span>
+                <span className="gray">{cardData.location}</span>
             </div>
             <div className="card-info">
-                <h2 className="card-title">{title}</h2>
-                <p><span className="bold">From ${price}</span> / person</p>
+                <h2 className="card-title">{cardData.title}</h2>
+                <p><span className="bold">From ${cardData.price}</span> / person</p>
             </div>
         </div>
     )
