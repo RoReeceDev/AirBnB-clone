65f97f53ab6f6b59e673078eb9ca59b172226ab3
RoReeceDev <roreecedev@gmail.com>
2024-01-23 19:56:56 -0500

    update to props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import Star from '../assets/star.png'
 import Katie from '../assets/katie-zaferes.png'
 
-const Card = () => {
+const Card = ({ img, rating, reviewCount, country, title, price }) => {
+
     return (
         <div className='card'>
             <div>
                 {/* <span>Sold Out</span> */}
-                <img className="card-feature" src={Katie} />
+                <img className="card-feature" src={`/src/assets/${img}`} alt="Main Card Image."/>
             </div>
             <div className="card-rating">
-                <img className="card-star"src={Star} />
-                <span>5.0</span>
-                <span className="gray">(6) &bull;</span>
-                <span className="gray">USA</span>
+                <img className="card-star"src={Star} alt="Rating Star Icon." />
+                <span>{rating}</span>
+                <span className="gray">({reviewCount}) &bull;</span>
+                <span className="gray">{country}</span>
             </div>
             <div className="card-info">
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <h2 className="card-title">{title}</h2>
+            <p><span className="bold">From ${price}</span> / person</p>
             </div>
         </div>
     )
